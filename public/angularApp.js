04bf8d2b99a17545f7e9863b42359b8193c29bdb
elksoneller <elksoneller@gmail.com>
2017-10-02 21:09:10 -0300

    Estrutura de projeto para post

--- public/angularApp.js
+++ public/angularApp.js
@@ -6,22 +6,21 @@ var listaContatos = angular.module('gamaBlogApp', []);
 function mainController($scope, $http) {  
 
     $scope.form = {};
-     
+         
     // Quando clicar no botão Criar, envia informações para a API Node
     $scope.criarContato = function() {
         $scope.form.data = new Date();
         $scope.form.ip = '192.0.0.1';
 
-        $http.post('/contatos', $scope.form)
-            .success(function(data) {
-                // Limpa o formulário para criação de outros contatos
-                $scope.formContato = {};
-                $scope.contatos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });        
+        $http.post('/contatos', $scope.form).success(function(data) {
+            // Limpa o formulário para criação de outros contatos
+            $scope.formContato = {};
+            $scope.contatos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });        
         
     }
 }
