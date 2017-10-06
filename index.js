84a40b449612ec5bd1490a1ddcdd31d1fc222ebb
elksoneller <elksoneller@gmail.com>
2017-10-06 05:41:17 -0300

    Adicionado plugin para envio de email após cadastro do lead; correções nos botões de compartilhamento em todos os posts; Criada pasta para armazenar os ebooks.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const port = 3000; //porta padrão
 const mysql = require('mysql');
 const ipify = require('ipify');
+const nodemailer = require('nodemailer');
 
 //configurando o body parser para pegar POSTS mais tarde
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,19 +21,20 @@ const router = express.Router();
 app.use('/', router);
 // DEFININDO NOSSA ROTA PARA O ANGULARJS/FRONT-END =========
 router.get('/', function(req, res) {
-    // Carrega nossa view index.html que será a única da nossa aplicação
-    // O Angular irá lidar com as mudanças de páginas no front-end
-    res.sendFile(__dirname + '/public/index.html');
+	// Carrega nossa view index.html que será a única da nossa aplicação
+	// O Angular irá lidar com as mudanças de páginas no front-end
+	res.sendFile(__dirname + '/public/index.html');
 });
 router.get('/contatos', (req, res) =>{
-    execSQLQuery('SELECT * FROM Contatos', res);
+	execSQLQuery('SELECT * FROM Contatos', res);
 });
 router.post('/contatos', (req, res) =>{
 	const nome = req.body.nome.substring(0,150);
-    const email = req.body.email.substring(0,150);
-    const ip = req.body.ip.substring(0,20);
-    const data = req.body.data;
-    execSQLQuery(`INSERT INTO Contatos(Nome, Email, IP, Data) VALUES('${nome}','${email}','${ip}', '${data}')`, res);   
+	const email = req.body.email.substring(0,150);
+	const ip = req.body.ip.substring(0,20);
+	const data = req.body.data;
+	execSQLQuery(`INSERT INTO Contatos(Nome, Email, IP, Data) VALUES('${nome}','${email}','${ip}', '${data}')`, res);
+	sendMail(email, nome, req.body.idEbook);
 })
 
 //inicia o servidor
@@ -40,13 +42,13 @@ app.listen(port);
 console.log('API funcionando!');
 
 function execSQLQuery(sqlQry, res){
-  const connection = mysql.createConnection({
-	host     : 'mysql857.umbler.com',
-	port     : 41890,
-	user     : 'gamaroot',
-	password : 'greenforcegama',
-	database : 'moeda'
-});
+	const connection = mysql.createConnection({
+		host     : 'mysql857.umbler.com',
+		port     : 41890,
+		user     : 'gamaroot',
+		password : 'greenforcegama',
+		database : 'moeda'
+	});
 
 	connection.query(sqlQry, function(error, results, fields){
 		if(error) 
@@ -54,5 +56,64 @@ function execSQLQuery(sqlQry, res){
 		else
 		res.json(results);
 		connection.end();
+	});
+}
+
+function sendMail(dest, nome, id_ebook){
+	//moedainteligente@gmail.com
+	//senha: greenforcerocks
+	
+	// Generate test SMTP service account from ethereal.email
+	// Only needed if you don't have a real mail account for testing
+	nodemailer.createTestAccount((err, account) => {
+
+		// create reusable transporter object using the default SMTP transport
+		let transporter = nodemailer.createTransport({
+			pool: true,
+			host: 'smtp.gmail.com',
+			port: 465,
+			secure: true, // use TLS
+			auth: {
+				user: 'moedainteligente', // generated ethereal user
+				pass: 'wedrgvszhdqdubtn'  // generated ethereal password
+			}
+		});
+
+		if(id_ebook == 1){
+			var anexo = {
+				filename: 'E-book_moeda_inteligente.pdf',
+				path: 'pdf/E-book_moeda_inteligente.pdf',
+				contentType: 'application/pdf'
+			}
+		} else {
+			var anexo = {
+				filename: 'E-book_guia_bolso.pdf',
+				path: 'pdf/E-book_guia_bolso.pdf',
+				contentType: 'application/pdf'
+			}
+		}
+
+		// setup email data with unicode symbols
+		let mailOptions = {
+			from: 'moedainteligente@gmail.com', // sender address
+			to: dest, // list of receivers
+			subject: 'Aqui está o seu Ebook ✔', // Subject line
+			text: 'Hello world?', // plain text body
+			html: '<div style="text-align: center"><h1>'+ nome +' Gostou do E-book?</h1><h2>Então convide seus amigos para visitar nosso <a href="https://www.moedainteligente.com.br">Blog</a></h2></div>', // html body
+			attachments: [anexo]
+		};
+
+		// send mail with defined transport object
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				return console.log(error);
+			}
+			console.log('Message sent: %s', info.messageId);
+			// Preview only available when sending through an Ethereal account
+			console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+			// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+			// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+		});
 	});
 }
--- public/angularApp.js
+++ public/angularApp.js
@@ -5,46 +5,45 @@ var listaContatos = angular.module('gamaBlogApp', []);
  
 function mainController($scope, $http) {  
 
-    $scope.form = {};
-         
-    // Quando clicar no botão Criar, envia informações para a API Node
-    $scope.criarContato = function() {
-        
-        if($scope.formLead.$invalid){
-            if ($scope.formLead.$error.required){
-                if ($scope.formLead.$error.required[0].$name == "nome"){
-                    alert("Preencha o campo nome");
-                } else if($scope.formLead.$error.required[0].$name == "sobrenome"){
-                    alert("Preencha o campo sobrenome");
-                } else if($scope.formLead.$error.required[0].$name == "email"){
-                    alert("Preencha o campo email");
-                }
-            } else if($scope.formLead.$error.email){
-                alert("Formato de email inválido")
-            }
-        }else {
-            $scope.form.data = moment().format('YYYY-MM-DD HH:mm:ss');
-            $scope.form.nome = $scope.form.nome+ ' ' +$scope.form.sobrenome;
-            $http({
-                method: 'GET',
-                url: '//www.stupidwebtools.com/api/my_ip.json'
-            }).then(function successCallback(response) {
-                $scope.form.ip = response.data.my_ip.ip;
-                $http.post('/contatos', $scope.form).success(function(data) {
-                    $scope.form.nome = "";
-                    $scope.form.sobrenome = "";
-                    $scope.form.email = "";
-                    alert("Obrigado, vc receberá o seu e-book via email.");
-                })
-                .error(function(data) {
-                    console.log('Error: ' + data);
-                });
-            }, function errorCallback(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                console.log(response);
-            });
-        }
-        
-    }
+	$scope.form = {};
+		 
+	// Quando clicar no botão Criar, envia informações para a API Node
+	$scope.criarContato = function(id) {
+		if($scope.formLead.$invalid){
+			if ($scope.formLead.$error.required){
+				if ($scope.formLead.$error.required[0].$name == "nome"){
+					alert("Preencha o campo nome");
+				} else if($scope.formLead.$error.required[0].$name == "sobrenome"){
+					alert("Preencha o campo sobrenome");
+				} else if($scope.formLead.$error.required[0].$name == "email"){
+					alert("Preencha o campo email");
+				}
+			} else if($scope.formLead.$error.email){
+				alert("Formato de email inválido")
+			}
+		}else {
+			$scope.form.data = moment().format('YYYY-MM-DD HH:mm:ss');
+			$scope.form.nome = $scope.form.nome+ ' ' +$scope.form.sobrenome;
+			$scope.form.idEbook = id;
+			$http({
+				method: 'GET',
+				url: '//www.stupidwebtools.com/api/my_ip.json'
+			}).then(function successCallback(response) {
+				$scope.form.ip = response.data.my_ip.ip;
+				$http.post('/contatos', $scope.form).success(function(data) {
+					$scope.form.nome = "";
+					$scope.form.sobrenome = "";
+					$scope.form.email = "";
+					alert("Obrigado, vc receberá o seu e-book via email.");
+				})
+				.error(function(data) {
+					console.log('Error: ' + data);
+				});
+			}, function errorCallback(response) {
+				// called asynchronously if an error occurs
+				// or server returns response with an error status.
+				console.log(response);
+			});
+		}		
+	}
 }
